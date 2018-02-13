73c65198153c2ae37f1fd73364990799e1c70aaf
Gustaf Pahlevi <gustafpahlevi@gmail.com>
2018-02-13 17:57:23 +0700

    release 1

--- composition.js
+++ composition.js
@@ -1,10 +1,19 @@
 "use strict"
 const fs = require('fs')
 
+class Ingredient {
+    constructor(name, amount) {
+        this.name = name
+        this.amount = amount
+    }
+}
+
 class Cookie {
-    constructor(name) {
+    constructor(name, ingredients) {
         this.name = name
         this.status = "mentah"
+        this.ingredients = ingredients
+        this.has_sugar = false
     }
 
     bake() {
@@ -13,25 +22,22 @@ class Cookie {
 }
 
 class PeanutButter extends Cookie {
-    constructor(name) {
-        super(name)
-        this.ingredients = [];
+    constructor(name, ingredients) {
+        super(name, ingredients)
         this.peanut_count = 100
     }
 }
 
 class ChocolateChip extends Cookie {
-    constructor(name) {
-        super(name)
-        this.ingredients = [];
+    constructor(name, ingredients) {
+        super(name, ingredients)
         this.choc_chip_count = 200
     }
 }
 
 class OtherCookie extends Cookie {
-    constructor(name) {
-        super(name)
-        this.ingredients = [];
+    constructor(name, ingredients) {
+        super(name, ingredients)
         this.other_count = 150
     }
 }
@@ -39,20 +45,64 @@ class OtherCookie extends Cookie {
 class CookieFactory {
     static create(option) {
         let cookiesArr = []
-        let cookie
-        for (let i = 0; i < option.length; i++) {
-            if (option[i] === 'peanut butter') {
-                cookiesArr.push(new PeanutButter(option[i]))
-            } else if (option[i] === 'chocolate chip') {
-                cookiesArr.push(new ChocolateChip(option[i]))
-            } else {
-                cookiesArr.push(new OtherCookie(option[i]))
+        let nameArr = [];
+
+        for (let j = 0; j < option.length; j++) {
+            nameArr.push(option.join('||').split('||')[j].split(':')[0].split('=')[0].trim())
+        }
+
+        for (let i = 0; i < nameArr.length; i++) {
+            if (nameArr[i] === 'peanut butter') {
+                cookiesArr.push(new PeanutButter(nameArr[i], CookieFactory.getIngredient(option[0])))
+            } else if (nameArr[i] === 'chocolate chip') {
+                cookiesArr.push(new ChocolateChip(nameArr[i], CookieFactory.getIngredient(option[1])))
+            } else if (nameArr[i] === 'chocolate cheese') {
+                cookiesArr.push(new OtherCookie(nameArr[i], CookieFactory.getIngredient(option[2])))
+            } else if (nameArr[i] === 'chocolate butter') {
+                cookiesArr.push(new OtherCookie(nameArr[i], CookieFactory.getIngredient(option[3])))
             }
         }
-        console.log(cookiesArr)
+        return cookiesArr
     }
+
+    static getIngredient(option) {
+        let arrIngredients = [];
+        let splitOption = option.split('=')
+        let dataLength = splitOption[1].split(',')
+        for (let i = 0; i < dataLength.length; i++) {
+            let splitLength = dataLength[i].split(':')
+            let obj = new Ingredient(splitLength[1].trim(), splitLength[0].trim())
+            arrIngredients.push(obj)
+        }
+        return arrIngredients
+    }
+
+    static cookieRecommnedation(day, batch_of_cookies) {
+        let recomendedCookie = []
+        for (let i = 0; i < batch_of_cookies.length; i++) {
+           for (let j = 0; j < batch_of_cookies[i].ingredients.length; j++) {
+              if (batch_of_cookies[i].ingredients[j].name === 'sugar') {
+                  batch_of_cookies[i].has_sugar = true
+              }
+           }
+        }
+
+        if (day === 'tuesday') {
+            for (let i = 0; i < batch_of_cookies.length; i++) {
+               if (batch_of_cookies[i].has_sugar === false) {
+                   recomendedCookie.push(batch_of_cookies[i]);
+               }
+            }
+        }
+        return recomendedCookie
+    }    
 }
 
 let option = fs.readFileSync('cookies.txt', 'utf-8').split('\n')
 let batch_of_cookies = CookieFactory.create(option);
-console.log(batch_of_cookies);
+
+let sugarFreeFoods = CookieFactory.cookieRecommnedation("tuesday", batch_of_cookies)
+console.log("sugar free cakes are: ")
+for (let i = 0; i < sugarFreeFoods.length; i++) {
+    console.log(sugarFreeFoods[i].name)    
+}
